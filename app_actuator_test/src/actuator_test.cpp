84e266f2174007c446389517d3404abc349421a8
aurel <appiusa@student.ethz.ch>
2022-11-04 11:42:29 +0100

    added logging functionality

--- app_actuator_test/src/actuator_test.cpp
+++ app_actuator_test/src/actuator_test.cpp
@@ -17,6 +17,7 @@
 using namespace mavsdk;
 using std::chrono::milliseconds;
 using std::chrono::seconds;
+using std::this_thread::sleep_for;
 
 int main(int argc, char **argv)
 {
@@ -58,39 +59,111 @@ int main(int argc, char **argv)
   /* ARM PLANE */
   const auto arm_result = action.arm();
 
+  /* MAVLINK PASSTROUGH */
+  mavsdk::MavlinkPassthrough::CommandLong cmd;
+  cmd.command = MAV_CMD_DO_SET_ACTUATOR;
+  cmd.target_sysid = mavlink.get_target_sysid();
+  cmd.param1 = 0;
+  cmd.param2 = 0.5;
+  cmd.param3 = 0;
+  cmd.param4 = 0;
+  cmd.param5 = 0;
+  cmd.param6 = 0;
+  cmd.param7 = 0;
+
+  const mavsdk::MavlinkPassthrough::CommandLong conscmd = cmd;
+
+  for (int i = 0; i < 10; i++)
+  {
+    mavlink.send_command_long(conscmd);
+    sleep_for(milliseconds(500));
+  }
+  // /* ATTITUDE OFFBOARD */
+  // std::cout << "Starting Offboard attitude control\n";
+
+  // // Send it once before starting offboard, otherwise it will be rejected.
+  // Offboard::Attitude att_msg{};
+  // att_msg.roll_deg = 0;
+
+  // Offboard::Result offboard_result = offboard.start();
+  // if (offboard_result != Offboard::Result::Success)
+  // {
+  //   std::cerr << "Offboard start failed: " << offboard_result << '\n';
+  //   return false;
+  // }
+  // std::cout << "Offboard started\n";
+
+  // std::cout << "Roll 30 degrees to the right\n";
+  // offboard.set_attitude(att_msg);
+  // sleep_for(seconds(10));
+
+  // offboard_result = offboard.stop();
+  // if (offboard_result != Offboard::Result::Success)
+  // {
+  //   std::cerr << "Offboard stop failed: " << offboard_result << '\n';
+  //   return false;
+  // }
+  // std::cout << "Offboard stopped\n";
+
   /* STARTING OFFBOARD */
   // construct actuator control group message right to evade Segmenation faults.
-  Offboard::ActuatorControlGroup grp;
+  std::vector<float>
+      grp;
   for (int i = 0; i < 8; i++)
   {
-    grp.controls.push_back(0.0);
+    grp.push_back(0);
   }
-  std::this_thread::sleep_for(milliseconds(2000));
-
   Offboard::ActuatorControl act_cmd{};
-  act_cmd.groups.push_back(grp);
-  act_cmd.groups.push_back(grp);
+  Offboard::ActuatorControlGroup grp1{};
+  Offboard::ActuatorControlGroup grp2{};
+  grp1.controls = grp;
+  grp2.controls = grp;
+  act_cmd.groups.push_back(grp1);
+  act_cmd.groups.push_back(grp2);
+  // Offboard::ActuatorControlGroup grp2{};
+  std::cout << "starting offboard" << std::endl;
   offboard.set_actuator_control(act_cmd);
+  std::cout << "started offboard" << std::endl;
   // start offboard
   Offboard::Result offboard_result = offboard.start();
+  std::cout << offboard_result << std::endl;
 
+  // act_cmd.groups.push_back(grp2);
   std::this_thread::sleep_for(milliseconds(500));
-  // act_cmd.groups.at(0).controls.at(3) = 0.25;
-  // std::this_thread::sleep_for(milliseconds(2500));
-  /* GO THROUGHT ALL THROTTLES FROM 0 TO 1 */
-  for (float throttle = 0.05; throttle <= 1.05; throttle += 0.05)
-  {
-    // send thrust command
-    std::cout << "now setting throttle = " << throttle << std::endl;
-    act_cmd.groups.at(0).controls.at(3) = throttle;
-    offboard.set_actuator_control(act_cmd);
 
-    // wait for thrust to settle
-    std::this_thread::sleep_for(milliseconds(10000));
-  }
+  // std::this_thread::sleep_for(milliseconds(2500));
+
+  /* GO THROUGHT ALL ANGLES FROM -1 TO 1 */
+  // for (float u = 0; u <= 1; u += 0.05)
+  // {
+  //   // send thrust command
+  //   std::cout << "now setting input to = " << u << std::endl;
+  //   act_cmd.groups.at(1).controls.at(0) = u;
+  //   std::cout << act_cmd.groups.at(0).controls.at(1) << std::endl;
+
+  //   offboard.set_actuator_control(act_cmd);
+
+  //   // wait for thrust to settle
+  //   std::this_thread::sleep_for(milliseconds(500));
+  // }
+
+  /* GO THROUGHT ALL THROTTLES FROM 0 TO 1 */
+  // for (float throttle = 0.05; throttle <= 1.05; throttle += 0.05)
+  // {
+  //   // send thrust command
+  //   std::cout << "now setting throttle = " << throttle << std::endl;
+  //   act_cmd.groups.at(0).controls.at(3) = throttle;
+  //   offboard.set_actuator_control(act_cmd);
+
+  //   // wait for thrust to settle
+  //   std::this_thread::sleep_for(milliseconds(10000));
+  // }
+
+  // stop offboard
+  offboard_result = offboard.stop();
 
   /* DISARM PLANE */
-  std::this_thread::sleep_for(seconds(5));
+  std::this_thread::sleep_for(seconds(2));
   const auto disarm_result = action.disarm();
   std::cout << "Disarming Result: " << disarm_result << '\n';
   myLog.close();
---
+++ app_flight_logger/src/flight_logger.cpp
@@ -0,0 +1,147 @@
+#include <chrono>
+#include <thread>
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <future>
+#include <ctime>
+// MAVSDK
+#include <mavsdk/mavsdk.h>
+#include <mavsdk/plugins/action/action.h>
+#include <mavsdk/plugins/mavlink_passthrough/mavlink_passthrough.h>
+#include <mavsdk/plugins/offboard/offboard.h>
+#include <mavsdk/plugins/telemetry/telemetry.h>
+
+// helpers
+#include "mavsdk_helper.h"
+
+using namespace mavsdk;
+using std::chrono::milliseconds;
+using std::chrono::seconds;
+
+/* PARAMETERS */
+int freq = 100; // logging frequency
+
+/* GLOBAL VARIABLES */
+Telemetry::ActuatorControlTarget act_targ;
+Telemetry::ActuatorOutputStatus act_out;
+float airspeed;
+
+/* CALLBACK FUNCTIONS */
+std::function<void(Telemetry::ActuatorControlTarget)> actuatorControlTargetCallback =
+    [](Telemetry::ActuatorControlTarget msg)
+{
+  act_targ = msg;
+};
+
+std::function<void(Telemetry::ActuatorOutputStatus)> actuatorOutputStatusCallback =
+    [](Telemetry::ActuatorOutputStatus msg)
+{
+  act_out = msg;
+};
+
+std::function<void(const mavlink_message_t &)> airSpeedCallback =
+    [](const mavlink_message_t &msg)
+{
+  airspeed = mavlink_msg_vfr_hud_get_airspeed(&msg);
+};
+
+int main(int argc, char **argv)
+{
+  /* INITIALIZE LOGGING */
+  std::ofstream myLog;
+  auto timenow = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
+  std::string Date = ctime(&timenow);
+  std::replace(Date.begin(), Date.end(), ' ', '_');
+  std::replace(Date.begin(), Date.end(), ':', '_');
+  Date.pop_back();
+  myLog.open("app_flight_logger/log/Flight_" + Date + ".csv");
+  std::cout << "Started logging to log/"
+            << "Flight_" << Date << ".csv" << std::endl;
+
+  myLog << "hey"
+        << "\n";
+  myLog.close();
+  std::cout << "closed log" << std::endl;
+  /* INITIALIZE MAVSDK */
+  if (argc != 2)
+  {
+    usage(argv[0]);
+    return 1;
+  }
+
+  Mavsdk mavsdk;
+  ConnectionResult connection_result = mavsdk.add_any_connection(argv[1]);
+
+  if (connection_result != ConnectionResult::Success)
+  {
+    std::cerr << "Connection failed: " << connection_result << '\n';
+    return 1;
+  }
+
+  auto system = get_system(mavsdk);
+  if (!system)
+  {
+    return 1;
+  }
+
+  auto telemetry = Telemetry{system}; // for telemetry services
+  auto mavlink = MavlinkPassthrough{system};
+  std::cout << "System is ready\n";
+
+  std::this_thread::sleep_for(milliseconds(500));
+
+  /* START LOGGING */
+  float t_s = 1 / float(freq);
+
+  /* SUBSCRIBER TO CALLBACK */
+  telemetry.subscribe_actuator_control_target(actuatorControlTargetCallback);
+  telemetry.subscribe_actuator_output_status(actuatorOutputStatusCallback);
+  mavlink.subscribe_message_async(74, airSpeedCallback);
+
+  std::vector<float> act_targs{0, 0, 0, 0, 0, 0, 0, 0};
+  for (float t = 0;; t += t_s)
+  {
+    // std::cout << "TARGET: group: " << act_targ.group << " size: " << act_targ.controls.size() << std::endl;
+    if (act_targ.controls.size() == 8)
+    {
+      for (int i = 0; i < 8; i++)
+      {
+        act_targs = act_targ.controls;
+      }
+      act_targs = {0, 0, 0, 0, 0, 0, 0, 0};
+    }
+    // std::cout << "airspeed: " << airspeed << std::endl;
+    //<< act_targ.controls.at(1) << "\t" << act_targ.controls.at(2) << "\t" << act_targ.controls.at(3) << std::endl;
+    //   write to log file
+    myLog << t << ","
+          << telemetry.position_velocity_ned().position.north_m << ","
+          << telemetry.position_velocity_ned().position.east_m << ","
+          << telemetry.position_velocity_ned().position.down_m << ","
+          << telemetry.position_velocity_ned().velocity.north_m_s << ","
+          << telemetry.position_velocity_ned().velocity.east_m_s << ","
+          << telemetry.position_velocity_ned().velocity.down_m_s << ","
+          << telemetry.attitude_euler().roll_deg << ","
+          << telemetry.attitude_euler().pitch_deg << ","
+          << telemetry.attitude_euler().yaw_deg << ","
+          << telemetry.attitude_angular_velocity_body().roll_rad_s << ","
+          << telemetry.attitude_angular_velocity_body().pitch_rad_s << ","
+          << telemetry.attitude_angular_velocity_body().yaw_rad_s << ","
+          << act_targs.at(0) << ","
+          << act_targs.at(1) << ","
+          << act_targs.at(2) << ","
+          << act_targs.at(3) << ","
+          << act_targs.at(4) << ","
+          << act_targs.at(5) << ","
+          << act_targs.at(6) << ","
+          << act_targs.at(7) << ","
+          << airspeed << "\n";
+
+    //  ensure logging frequency
+    std::this_thread::sleep_for(milliseconds(int(1000 / freq)));
+  }
+
+  /* CLOSE LOG */
+  myLog.close();
+  return 0;
+}
